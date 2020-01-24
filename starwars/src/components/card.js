940f26152293fc42c4d4eedabcedf095a7532444
carlsachs <sachscarl@gmail.com>
2020-01-24 11:50:22 -0500

    added some component pages, and created the axios call

---
+++ starwars/src/components/card.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Card = props => {
+    // eslint-disable-next-line no-unused-expressions
+    <divWrap>
+        <h2>Name: {props.data.name}</h2>
+        <p>Height: {props.data.name}</p>
+    </divWrap>
+}
+export default Card;
---
+++ starwars/src/components/container.js
@@ -0,0 +1,31 @@
+import React, { useState , useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import Card from "./card"
+
+export default function Container () {
+    const [star, setStar] = useState([]);
+    useEffect(() => {
+        axios.get(`https://swapi.co/api/people`)
+        .then(res => {
+            console.log(res);
+            setStar(res.data);
+        })
+        .catch(err => {
+            console.log("Data cannot be returned", err);
+        });
+    }, []);
+
+    return (
+
+        <div>
+            <Card />
+            {data.map(p => (
+                <div>
+                  <Post  post={p}/>
+                </div>
+          ))}
+        </div>
+
+    )
+}
